74fc1c5990cb14e327e04cb8f1a757f255b122d3
Vishal Shah <shahvishal662@gmail.com>
2021-08-31 22:16:47 +0530

    changed the production website link in server config

--- config.js
+++ config.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : "https://youwebsite.com";
+export const server = dev
+  ? "http://localhost:3000"
+  : "https://paku-components-nextjs.netlify.app";
