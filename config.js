844f906200ebf4c39779a83356706bb9106c2f0a
Vishal Shah <shahvishal662@gmail.com>
2021-09-01 22:41:19 +0530

    changed the prod site to netlify link in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://pakucomponents.com/nextjs";
+  : "https://paku-components-nextjs.netlify.app";
